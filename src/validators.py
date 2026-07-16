ed8f95457709e3cb91da3c5d4ef0fe2e8ff56c79
JavierRichart <lopezrichart@gmail.com>
2026-07-16 21:39:31 +0200

    Use of validation_result

--- src/validators.py
+++ src/validators.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from src.base_validator import BaseValidator
+from src.validation_result import ValidationResult
 
 
 class RequiredColumnsValidator(BaseValidator):
@@ -8,21 +9,43 @@ class RequiredColumnsValidator(BaseValidator):
         self.required_columns = required_columns
 
 
-    def validate(self, dataframe: pd.DataFrame) -> list[str]:
-        return [
-            column
+    def validate(
+            self,
+            dataframe: pd.DataFrame,
+    ) -> ValidationResult:
+        
+        missing_columns = [
+            column 
             for column in self.required_columns
             if column not in dataframe.columns
         ]
 
+        return ValidationResult(
+            name="required_columns",
+            passed=not missing_columns,
+            details=missing_columns
+        )
+    
 
 class DuplicateColumnsValidator(BaseValidator):
-    def validate(self, dataframe: pd.DataFrame) -> list[str]:
-        return dataframe.columns[
+    def validate(self, dataframe: pd.DataFrame) -> ValidationResult:
+        duplicated = dataframe.columns[
             dataframe.columns.duplicated()
         ].tolist()
+
+        return ValidationResult(
+            name="duplicated_columns",
+            passed=not duplicated,
+            details=duplicated,
+        )
     
 
 class NullValuesValidator(BaseValidator):
-    def validate(self, dataframe: pd.DataFrame) -> dict[str, int]:
-        return dataframe.isnull().sum().to_dict()
+    def validate(self, dataframe: pd.DataFrame) -> ValidationResult:
+        null_data =  dataframe.isnull().sum().to_dict()
+
+        return ValidationResult(
+            name="null_data",
+            passed=not any(null_data.values()),
+            details=null_data
+        )
